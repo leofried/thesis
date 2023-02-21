7b1a08057068156fccf9a2735bb3297cc54aba39
Leo <leofriedmail@gmail.com>
2023-02-21 16:43:43 -0500

    playing around with speedups

--- prog/simulate.ml
+++ prog/simulate.ml
@@ -17,7 +17,7 @@ let rec run_sims (scheme : Scheme.t) (iters_left : int) (decays: float list) (se
   run_sims scheme (iters_left - 1) (decay :: decays) seed_wins
 ;;
 
-let get_data ~(iters : int) (scheme : Scheme.t) : Data.t =
+let sim_scheme ~(iters : int) (scheme : Scheme.t) : Data.t =
   let seed_wins = Array.make (Scheme.number_of_teams scheme) 0 in
   let sims = run_sims scheme iters [] seed_wins in
   {
@@ -28,7 +28,7 @@ let get_data ~(iters : int) (scheme : Scheme.t) : Data.t =
     scheme = scheme;
   }
 ;;
-
+(*
 let sim_scheme ~(iters : int) (lst : Data.t list) (scheme : Scheme.t) : Data.t list =
   let new_data = get_data ~iters scheme in
   let found, new_lst = List.fold_left_map
@@ -44,7 +44,7 @@ let sim_scheme ~(iters : int) (lst : Data.t list) (scheme : Scheme.t) : Data.t l
   if found then new_lst else new_data :: new_lst
 ;;
 
-
+*)
 let sim_schemes ~(luck : float) ~(iters_pow : int) (schemes : Scheme.t list) : unit =
   if iters_pow < 1 then assert false;
   Team.set_luck luck;
@@ -53,16 +53,15 @@ let sim_schemes ~(luck : float) ~(iters_pow : int) (schemes : Scheme.t list) : u
   let count = ref 0 in
   let total = (List.length schemes) in
 
-  List.fold_left 
-    (fun data (scheme : Scheme.t) ->
+  List.map
+    (fun (scheme : Scheme.t) ->
       if (Scheme.number_of_teams scheme) <> number_of_teams then assert false;
       Math.inc_ref count;
       print_endline @@ (Int.to_string !count) ^ "/" ^ Int.to_string total ^ ": " ^ Scheme.name scheme;
-      sim_scheme ~iters:(Math.pow 10 iters_pow) data scheme
+      sim_scheme ~iters:(Math.pow 10 iters_pow) scheme
     )
-    (Data.read ~luck ~number_of_teams false)
     schemes
-  |> Data.write ~luck ~number_of_teams  
+  |> Data.write ~luck ~number_of_teams
 ;;
 
 let rec sim_smart  ~(luck : float) ~(number_of_teams : int) ~(max_games : int) ~(iters_pow : int) ~(batch_size : int) : unit =
@@ -76,15 +75,10 @@ let rec sim_smart  ~(luck : float) ~(number_of_teams : int) ~(max_games : int) ~
     | _ :: tl -> p_helper tl data
   in
 
-  let data = Data.read ~luck ~number_of_teams ~max_games true in
-  let total = List.fold_left (fun x (d : Data.t) -> x + d.iters) 0 data in
-  
-  data
+  Data.read ~luck ~number_of_teams ~max_games true
   |> List.map (p_helper pareto_list) 
   |> Stats.sample batch_size
   |> sim_schemes ~luck ~iters_pow;
-
-  print_endline @@ "Total sims = " ^ Int.to_string (total + (Math.pow 10 iters_pow) * batch_size);
 
   sim_smart ~luck ~number_of_teams ~max_games ~iters_pow ~batch_size
 ;;
---
+++ util/debug.ml
@@ -0,0 +1,2 @@
+let mark s x = print_endline s; x;;
+let hold p x = p x; x;;
